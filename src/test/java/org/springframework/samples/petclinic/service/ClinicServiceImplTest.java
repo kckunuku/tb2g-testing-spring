47edce80de55940e70dce2a229e2af30012a9620
Kunuku, Krishna <krishna.kunuku@optum.com>
2020-05-11 00:47:08 -0500

    first round of tests

---
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceImplTest.java
@@ -0,0 +1,11 @@
+package org.springframework.samples.petclinic.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClinicServiceImplTest {
+
+    @Test void findPetTypes() {
+    }
+}
